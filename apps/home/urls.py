b04cc0c899617eadd8ce7f04a88911c81c12cf8b
Pablo Acosta <acosta13233@hotmail.com>
2022-04-21 16:13:56 -0600

    diseno y index al dashboard

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,5 +4,12 @@ from . import views
 app_name = "home_app"
 
 urlpatterns = [
-    path('', views.IndexHomeView.as_view(), name='index')
+    path('', 
+        views.IndexHomeView.as_view(), 
+    name='index',
+    ),
+    path('administracion', 
+        views.DashboardIndexView.as_view(), 
+    name='dashboard_index',
+    ),
 ]
--- distribuidora/urls.py
+++ distribuidora/urls.py
@@ -4,9 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.home.urls')),
-    path('', include('apps.categoria.urls')),
+    path('admin/', 
+        admin.site.urls
+    ),
+    path('', 
+        include('apps.home.urls'),
+    ),
+    path('', 
+        include('apps.categoria.urls'),
+    ),
     #path('', include('apps.carrusel.urls')),
-    path('', include('apps.producto.urls')),
+    path('', 
+        include('apps.producto.urls'),
+    ),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
