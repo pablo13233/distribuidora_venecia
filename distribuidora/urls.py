c29a740207021e7ef582a693ae20ea6f89b6746d
Yeltsin <yeltsinbaquedano@gmail.com>
2022-04-22 17:13:17 -0600

    Integracion para plantilla de administracion

--- distribuidora/urls.py
+++ distribuidora/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
         include('apps.home.urls'),
     ),
     path('', 
-        include('apps.categoria.urls'),
+        include(('apps.categoria.urls'), namespace='categoria'),
     ),
     #path('', include('apps.carrusel.urls')),
-    path('', include('apps.producto.urls')),
-    path('', include('apps.marca.urls')),
+    path('', include(('apps.producto.urls'), namespace='producto')),
+    path('', include(('apps.marca.urls'), namespace='marca')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
