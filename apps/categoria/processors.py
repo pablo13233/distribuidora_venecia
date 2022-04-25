e99b96b425f15247181ae2cb8723b15c1b7b8ce7
Pablo Acosta <acosta13233@hotmail.com>
2022-04-25 16:25:07 -0600

    processors

---
+++ apps/categoria/processors.py
@@ -0,0 +1,7 @@
+def categoria_list(request):
+    context_data = dict()
+    
+"""             context_data["cc_secciones"] = Section.objects.filter(
+                parentisnull=True, publish=True, groupin=user_groups
+            ).order_by("order")
+    return context_data """
