13d7bcd2c08ea453ff9b96349a2078c9d16aa428
Pablo Acosta <acosta13233@hotmail.com>
2022-04-25 18:40:22 -0600

    funciones para filtros en index y navbar

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,12 +1,24 @@
+
 from django.shortcuts import render
 from django.views.generic import (
     TemplateView
 )
 
+from apps.producto.models import Producto
+from apps.carrusel.models import Carrusel
+
 # Create your views here.
 
 class IndexHomeView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        productos = Producto.objects.all().order_by('pk')[:5]
+        carrusel = Carrusel.objects.all().order_by('pk')[:3]
+        context['imagenes'] = carrusel
+        context['last_productos'] = productos
+        return context
 
 class DashboardIndexView(TemplateView):
     template_name = "dashboard/dashboard_index.html"
